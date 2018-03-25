6b2adba65dcf7a5270aca79ca822ccbe23aa0d43
wangyong <yundan_wy@126.com>
2018-03-25 21:21:27 +0800

    spring initializr

---
+++ src/main/java/com/initializr/InitializrApplication.java
@@ -0,0 +1,18 @@
+package com.initializr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 程序入口
+ *
+ * @author wangyong
+ * @date 2018年03月25日
+ */
+@SpringBootApplication
+public class InitializrApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(InitializrApplication.class, args);
+  }
+}
